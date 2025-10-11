01be2b7b03369ee28cbc7144fadf0b0bbf9533d6
jayce <lukasoperio.12@gmail.com>
2025-10-11 14:00:22 +0800

    inayos ko na db connection

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "127.0.0.1"; // safer than 'localhost' for custom ports
-$username   = "xampp";
-$password   = "Phpxampp809";
-$dbname     = "flakies";
-$port       = 3306; // XAMPP MySQL port
+    $servername = "127.0.0.1"; // safer than 'localhost' for custom ports
+    $username   = "root";
+    $password   = "";
+    $dbname     = "flakies";
+    $port       = 3307; // XAMPP MySQL port
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Check connection
-if ($conn->connect_errno) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // Check connection
+    if ($conn->connect_errno) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
