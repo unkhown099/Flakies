d2b03e9a5501394399a65d5057bbce5b91e8b3e9
unkhown099 <167945473+unkhown099@users.noreply.github.com>
2025-10-20 15:46:48 +0800

    change it to 3306

--- config/db_connect.php
+++ config/db_connect.php
@@ -3,7 +3,7 @@ $servername = "127.0.0.1"; // safer than 'localhost' for custom ports
 $username   = "root";
 $password   = "";
 $dbname     = "flakies";
-$port       = 3307; // XAMPP MySQL port
+$port       = 3306; // XAMPP MySQL port
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
